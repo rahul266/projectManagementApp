596e533757393cb266f4f310051ae44f72152f8a
rahul266 <rahulvemula03@gmail.com>
2024-02-01 15:56:40 +0530

    redirection issue is fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { env } from './env';
 
 export default async function authenticationMiddleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
     if (path === '/') {
         return NextResponse.next();
     }
-    const session = request.cookies.get("next-auth.session-token")
+    let session
+    if (env.NODE_ENV === 'development') {
+        session = request.cookies.get("next-auth.session-token")
+    } else {
+        session = request.cookies.get("__Secure-next-auth.session-token")
+    }
 
     const isProtected = path.includes('/projects') || path.includes('/teams') || path.includes('/ticket')
         || path.includes('/project');
