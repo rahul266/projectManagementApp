b58c479ae5f25b91eecfb4459de3ffd1f92dfe8d
rahul266 <rahulvemula03@gmail.com>
2024-02-02 00:05:33 +0530

    set new things on nav bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,22 +5,31 @@ import { useRouter } from 'next/router';
 
 const Navbar: React.FC = () => {
     const sessionData = useSession()
-    const router=useRouter()
+    const router = useRouter()
+    const isHome = router.pathname === '/';
+    const isProjects = router.pathname.includes('/projects')
+    const isTeams=router.pathname.includes('/teams')
     return (
         <nav className="bg-stone-200 p-4">
             <div className="flex justify-between items-center">
                 <div className="flex items-center">
                     <div className="text">
-                            <p className="text-lg font-bold mx-5">App Logo</p>
+                        <Image
+                            src="/appLogo.png"
+                            alt="some-image"
+                            width={50}
+                            height={50}
+                            className=" mx-5 cursor-pointer">
+                        </Image>
                     </div>
                     <div className="flex space-x-4 mx-10">
                         <Link legacyBehavior href="/">
-                            <a className="text">Home</a>
+                            <a className={`text ${isHome ? 'text-orange-500 font-extrabold' : 'hover:text-orange-500 hover:font-semibold'}`}>Home</a>
                         </Link>
-                        <Link legacyBehavior href="/projects">
-                            <a className="text">Projects</a>
+                        <Link legacyBehavior  href="/projects">
+                            <a className={`text ${isProjects ? 'text-orange-500 font-extrabold' : 'hover:text-orange-500 hover:font-semibold'}`}>Projects</a>
                         </Link>
-                        <div className="cursor-pointer" onClick={() => { router.push(`/teams/${sessionData.data?.user.id}`) }}>
+                        <div className={`text ${isTeams ? 'cursor-pointer text-orange-500 font-extrabold' : 'cursor-pointer hover:text-orange-500 hover:font-semibold'}`}  onClick={() => { router.push(`/teams/${sessionData.data?.user.id}`) }}>
                             <a className="text">Teams</a>
                         </div>
                     </div>
